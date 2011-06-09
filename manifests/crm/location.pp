3571fff40803aec2f3b7a80f31c7e6328a4e5016
Arthur Holstvoogd <a.holstvoogd@nedforce.nl>
2011-06-09 17:31:13 +0200

    Remove checks from location.pp. They don't work.

--- manifests/crm/location.pp
+++ manifests/crm/location.pp
@@ -15,13 +15,11 @@ define ha::crm::location($ensure=present, $resource, $score, $rule = '', $host =
 	if($ha_cluster_dc == $hostname) or ($ha_cluster_dc == $fqdn) or ($ignore_dc == "true") {
 		if($ensure == absent) {
 			exec { "Removing location rule ${name}":
-				command => "/usr/sbin/crm configure location delete ${name}",
-				onlyif  => "/usr/sbin/crm configure show location ${name} > /dev/null 2>&1",
+				command => "/usr/sbin/crm configure location delete ${name}"
 			}
 		} else {
 			exec { "Creating location rule ${name}":
-				command => "/usr/sbin/crm configure location ${name} ${resource} ${loc}",
-				unless  => "/usr/sbin/crm configure show location ${name} > /dev/null 2>&1",
+				command => "/usr/sbin/crm configure location ${name} ${resource} ${loc}"
 			}
 		}
 	}
