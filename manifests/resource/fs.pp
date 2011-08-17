d564e27b1babfd01eb436fd0c72db8cda08f9e95
Daniel van der Veer <d.f.vanderveer@nedforce.nl>
2011-08-17 13:37:11 +0200

    fix exists method and ensure in two resources

--- manifests/resource/fs.pp
+++ manifests/resource/fs.pp
@@ -2,32 +2,34 @@ define ha::resource::fs($device, $fstype = 'ext3', $directory = "/mnt/${name}", 
   ha_crm_primitive {
     "${name}": 
       type    => "ocf:heartbeat:Filesystem",
-      ensure  => present;
+      ensure  => $ensure;
   }
-  ha_crm_parameter { 
-    "${name}-device":
-      ensure    => present,
-      resource  => "${name}",
-      key       => "device",
-      value     => "${device}",
-      require   => Ha_Crm_Primitive["${name}"];
-    "${name}-type":
-      ensure    => present,
-      resource  => "${name}",
-      key       => "fstype",
-      value     => "${fstype}",
-      require   => Ha_Crm_Primitive["${name}"];
-    "${name}-directory":
-      ensure    => present,
-      resource  => "${name}",
-      key       => "directory",
-      value     => "${directory}",
-      require   => Ha_Crm_Primitive["${name}"];
-    "${name}-options":
-      ensure    => present,
-      resource  => "${name}",
-      key       => "options",
-      value     => "${options}",
-      require   => Ha_Crm_Primitive["${name}"];
+  if ( $ensure != absent ) {
+    ha_crm_parameter { 
+      "${name}-device":
+        ensure    => present,
+        resource  => "${name}",
+        key       => "device",
+        value     => "${device}",
+        require   => Ha_Crm_Primitive["${name}"];
+      "${name}-type":
+        ensure    => present,
+        resource  => "${name}",
+        key       => "fstype",
+        value     => "${fstype}",
+        require   => Ha_Crm_Primitive["${name}"];
+      "${name}-directory":
+        ensure    => present,
+        resource  => "${name}",
+        key       => "directory",
+        value     => "${directory}",
+        require   => Ha_Crm_Primitive["${name}"];
+      "${name}-options":
+        ensure    => present,
+        resource  => "${name}",
+        key       => "options",
+        value     => "${options}",
+        require   => Ha_Crm_Primitive["${name}"];
+    }
   }
 }
