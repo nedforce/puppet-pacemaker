d782b9b8ba733564b5439b2098e7c1125344435c
Arthur Holstvoogd <a.holstvoogd@nedforce.nl>
2011-08-17 17:09:08 +0200

    fix node attr provider

--- lib/puppet/provider/ha_crm_node_attribute/crm.rb
+++ lib/puppet/provider/ha_crm_node_attribute/crm.rb
@@ -3,7 +3,7 @@ Puppet::Type.type(:ha_crm_node_attribute).provide(:crm) do
   commands :crm => "crm"
 
   def create
-    crm "node", "attribute", source[:host], "set", resource[:attribute], resource[:value]
+    crm "node", "attribute", resource[:host], "set", resource[:attribute], resource[:value]
   end
 
   def destroy
@@ -14,7 +14,7 @@ Puppet::Type.type(:ha_crm_node_attribute).provide(:crm) do
     if resource[:only_run_on_dc] && !(Facter.value(:ha_cluster_dc) == Facter.value(:fqdn) || Facter.value(:ha_cluster_dc) == Facter.value(:hostname)) 
       true
     else
-      val = crm "node", "attribute", resource[:host], "show", resource[:name]
+      val = crm "node", "attribute", resource[:host], "show", resource[:name] rescue nil
       val == resource[:value]
     end
   end
