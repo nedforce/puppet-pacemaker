19beff0db5cd4b4d9747bd0504e9de69cef14e37
Daniel van der Veer <d.f.vanderveer@nedforce.nl>
2011-09-02 17:53:42 +0200

    do not remove always present primitive parameters

--- lib/puppet/provider/ha_crm_primitive/crm.rb
+++ lib/puppet/provider/ha_crm_primitive/crm.rb
@@ -79,7 +79,7 @@ Puppet::Type.type(:ha_crm_primitive).provide(:crm) do
 
   def target_role=(value)
     if value.to_s == "absent"
-      crm_resource "-m", "-r", resource[:id], "-d", "target-role"
+      #crm_resource "-m", "-r", resource[:id], "-d", "target-role"
     else
       crm_resource "-m", "-r", resource[:id], "-p", "target-role", "-v", value.to_s.capitalize
     end
@@ -102,7 +102,7 @@ Puppet::Type.type(:ha_crm_primitive).provide(:crm) do
 
   def is_managed=(value)
     if value.to_s == "absent"
-      crm_resource "-m", "-r", resource[:id], "-d", "is-managed"
+      #crm_resource "-m", "-r", resource[:id], "-d", "is-managed"
     else
       crm_resource "-m", "-r", resource[:id], "-p", "is-managed", "-v", value.to_s
     end
